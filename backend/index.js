e7421f610ffe1d3dded61a0799e118e168a83f34
Ghulam Qadir <connecttoqadir@gmail.com>
2023-09-05 19:29:22 +0500

    mongooose

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,20 @@
 const express = require('express')
 const app = express()
-const productRoutes = require('./routes/products')
-const customerRoutes = require('./routes/customers')
+const studentsRoutes = require('./routes/students')
+const mongoose = require('mongoose')
 
 
 
+app.use(express.json())
+app.use('/api/students',studentsRoutes)
 
-app.use('/api/products',productRoutes)
-app.use('/api/customers',customerRoutes)
 
 
+mongoose.connect('mongodb://127.0.0.1:27017/batch5').then((res)=>{
+    console.log("Connected!")
+}).catch((err)=>{
+    console.log(err.message)
+})
 
 
 
